1023b811c2e0d0284b30a2cc2ae1b56e5bf8d949
joe.zlonicky@gmail.com <joe.zlonicky@gmail.com>
2020-01-21 16:02:31 -0800

    Refactored and recolored

--- main.py
+++ main.py
@@ -2,63 +2,78 @@ import pygame
 import sys
 import math
 
-BLOCK_CHAR = '#'
-EMPTY_CHAR = '.'
-START_CHAR = 'A'
-END_CHAR = 'B'
-BG_COLOR = (0, 0, 0)
-BLOCK_COLOR = (255, 255, 255)
-LINE_COLOR = (75, 75, 75)
-PATH_COLOR = (255, 0, 0)
-START_COLOR = (0, 255, 0)
-END_COLOR = (0, 0, 255)
-BORDER_WIDTH = 1
-CELL_SIZE = 30
-FPS = 30
-
 
 class AStarVisual:
-    def __init__(self, size, map):
-        self.size = size
-        self.map = map
+    """ Visualizes the A* pathfind algorithm using an inputted map.
+        To run, redirect stdin to map.txt """
+    BLOCK_CHAR = '#'
+    EMPTY_CHAR = '.'
+    START_CHAR = 'A'
+    END_CHAR = 'B'
+    BG_COLOR = (36, 34, 52)
+    BLOCK_COLOR = (74, 84, 98)
+    LINE_COLOR = (51, 57, 65)
+    PATH_COLOR = (156, 219, 67)
+    START_COLOR = (89, 193, 53)
+    END_COLOR = (180, 32, 42)
+    BORDER_WIDTH = 1
+    CELL_SIZE = 30
+    FPS = 30
+
+    def __init__(self):
+        """ Start a new visualization """
+        self.size = 0
+        self.map = []
         self.start = None
         self.end = None
+        self.read_map()
+
+        self.screen = pygame.display.set_mode((self.size * self.CELL_SIZE + (self.size + 1) * self.BORDER_WIDTH,
+                self.size * self.CELL_SIZE + (self.size + 1) * self.BORDER_WIDTH))
+        pygame.display.set_caption("AStarVisual")
+        self.clock = pygame.time.Clock()
         self.queue = []
+        self.path = []
         self.cost_to_pos = {}
         self.previous = {}
-        self.screen = pygame.display.set_mode((size * CELL_SIZE + (size + 1) * BORDER_WIDTH,
-                size * CELL_SIZE + (size + 1) * BORDER_WIDTH))
-        self.clock = pygame.time.Clock()
         self.done = False
-        self.path = []
         self.begin()
 
-    def begin(self):
+    def read_map(self):
+        """ Get map from stdin. Expects first line to be the size of the map 
+                and the following lines to be the map itself """
+        input = list(sys.stdin)
+        self.size = int(input[0])
+        self.map = input[1:]
         for i in range(len(self.map)):
             if 'A' in self.map[i]:
-                self.start = (self.map[i].index(START_CHAR), i)
+                self.start = (self.map[i].index(self.START_CHAR), i)
             if 'B' in self.map[i]:
-                self.end = (self.map[i].index(END_CHAR), i)
+                self.end = (self.map[i].index(self.END_CHAR), i)
+
+    def begin(self):
+        """ Add start cell and begin loop """
         self.queue.append((self.start, 0.0))
         self.cost_to_pos[self.start] = 0
         self.loop()
 
     def loop(self):
+        """ Handle events, apply algorithm and draw to screen """
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     return
-
             if not self.done:
                 self.update_path()
                 self.algorithm()
 
-            self.screen.fill(LINE_COLOR)
+            self.screen.fill(self.LINE_COLOR)
             self.draw_board()
             pygame.display.flip()
-            self.clock.tick(FPS)
+            self.clock.tick(self.FPS)
 
     def algorithm(self):
+        """ Apply A* """
         current = self.get_next_cell()
         if not current or current == self.end:
             self.done = True
@@ -66,8 +81,8 @@ class AStarVisual:
 
         # Add neighbors to queue
         for neighbor in self.get_neighbors(current):
-            # Don't add blocked cells
-            if self.map[current[1]][current[0]] == BLOCK_CHAR:
+            # Don't add wall cells
+            if self.map[current[1]][current[0]] == self.BLOCK_CHAR:
                 break
 
             # Only add if not visited or a cheaper path has been found
@@ -81,6 +96,7 @@ class AStarVisual:
                 self.queue.append((neighbor, distance))
 
     def get_next_cell(self):
+        """ Get the highest priority cell """
         if len(self.queue) == 0:
             return
         val = self.peek_queue()
@@ -88,6 +104,7 @@ class AStarVisual:
         return val[0]
 
     def peek_queue(self):
+        """ Return the highest priority cell as a tuple: ((x, y), priority) """
         if len(self.queue) == 0:
             return
         max_priority = self.queue[0][1]
@@ -100,6 +117,7 @@ class AStarVisual:
         return val
 
     def get_neighbors(self, pos):
+        """ Get all adjacent cells as a list """
         neighbors = []
         if pos[0] + 1 < self.size:
             neighbors.append((pos[0] + 1, pos[1]))
@@ -112,6 +130,7 @@ class AStarVisual:
         return neighbors
 
     def update_path(self):
+        """ Update the cells that are in the path """
         if len(self.queue) == 0:
             return
         self.path[:] = []
@@ -121,23 +140,23 @@ class AStarVisual:
             current = self.previous[current]
 
     def draw_board(self):
+        """ Draw map to screen """
         for i in range(self.size):
             for k in range(self.size):
-                left = k * CELL_SIZE + (k+1) * BORDER_WIDTH
-                top = i * CELL_SIZE + (i+1) * BORDER_WIDTH
-                rect = pygame.Rect(left, top, CELL_SIZE, CELL_SIZE)
-                color = BG_COLOR
-                if self.map[i][k] == BLOCK_CHAR:
-                    color = BLOCK_COLOR
-                elif self.map[i][k] == START_CHAR:
-                    color = START_COLOR
-                elif self.map[i][k] == END_CHAR:
-                    color = END_COLOR
+                left = k * self.CELL_SIZE + (k+1) * self.BORDER_WIDTH
+                top = i * self.CELL_SIZE + (i+1) * self.BORDER_WIDTH
+                rect = pygame.Rect(left, top, self.CELL_SIZE, self.CELL_SIZE)
+                color = self.BG_COLOR
+                if self.map[i][k] == self.BLOCK_CHAR:
+                    color = self.BLOCK_COLOR
+                elif self.map[i][k] == self.START_CHAR:
+                    color = self.START_COLOR
+                elif self.map[i][k] == self.END_CHAR:
+                    color = self.END_COLOR
                 elif (k, i) in self.path:
-                    color = PATH_COLOR
+                    color = self.PATH_COLOR
                 pygame.draw.rect(self.screen, color, rect)
 
 
 if __name__ == '__main__':
-    input = list(sys.stdin)
-    AStarVisual(int(input[0]), input[1:])
+    AStarVisual()
